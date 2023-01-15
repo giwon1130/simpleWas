3494c0784014883e6c1490e5aac53ece48075d4e
임기원 <gwim1130@naver.com>
2023-01-15 17:57:29 +0900

    초기세팅

--- src/main/java/com/example/simplewas/SimplewasApplication.java
+++ src/main/java/com/example/simplewas/SimplewasApplication.java
@@ -2,8 +2,9 @@ package com.example.simplewas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class SimplewasApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/simplewas/TestController.java
+++ src/main/java/com/example/simplewas/TestController.java
@@ -1,2 +1,21 @@
-package com.example.simplewas;public class TestController {
+package com.example.simplewas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+
+    @GetMapping("/info")
+    public Map<String, Object> testInfoGET() {
+        Map<String, Object> resultMap = new HashMap<>();
+        resultMap.put("이름", "임기원");
+        resultMap.put("한줄요약", "적극적인 리더형 개발자");
+        return resultMap;
+    }
 }
